e3dd16eb16b7f7770bced0cba2838e6f10a678d1
Joost Baaij <joost@spacebabies.nl>
2016-08-18 09:40:58 +0200

    improve the initializer
    use the Sidekiq.configure_server hooks to schedule the worker.
    
    Thanks for the contribution @imme5150
    
    Fixes #6

--- config/initializers/perform.rb
+++ config/initializers/perform.rb
@@ -1,9 +1,7 @@
-# This initializer inspects the currently scheduled Sidekiq workers,
-# of which the Sidekiq::Snitch must always be one in a properly
-# configured application.
-#
-# When none are found, schedule one to run immediately. The worker
-# will then take care of rescheduling itself.
-if ! SidekiqSnitch.already_scheduled && ! ENV['SIDEKIQ_SNITCH_URL'].blank?
-  Sidekiq::Snitch.perform_async
+# Scheduled a Sidekiq::Snitch worker to monitor Sidekiq using
+# Dead Man's Snitch (www.deadmanssnitch.com).
+Sidekiq.configure_server do |config|
+  config.on(:startup) do
+    Sidekiq::Snitch.perform_async unless SidekiqSnitch.scheduled?
+  end
 end
